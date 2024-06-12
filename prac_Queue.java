f02c78a4521f1a1e4eb830ed5e9017bdf7dff94a
venkateshs05 <167161346+venkateshs05@users.noreply.github.com>
2024-06-12 13:41:03 +0530

    Create prac_Queue.java

---
+++ prac_Queue.java
@@ -0,0 +1,102 @@
+import java.util.Scanner;
+
+
+
+public class pr_6_Queue {
+int maxSize,front,rear,temp;
+int Queue[];
+    void create_queue(int size){
+        maxSize=size;
+        Queue=new int [maxSize];
+        front=0;
+        rear=-1;
+
+    }
+    void EnQueue(int k){
+        rear++;
+        Queue[rear]=k;
+        
+    }
+    boolean isFull(){
+        if(rear==maxSize-1){
+            return true;
+
+        }
+        else{
+            return false;
+        }
+    }
+    int DeQueue(){
+        temp=Queue[rear];
+        front++;
+        return temp;
+    }
+    boolean isEmpty(){
+        if(front>rear){
+            return true;
+        }
+        else{return false;}
+    }
+    void printQueue(){
+        for(int i=front;i<=rear;i++){
+            System.out.print(Queue[i]+"--");
+        }
+    }
+    public static void main(String[] args) {
+         int size,element,choice;
+        pr_6_Queue obj=new pr_6_Queue();
+        System.out.println("Enter the size of Queue");
+        Scanner sc=new Scanner(System.in);
+        size=sc.nextInt();
+        obj.create_queue(size);
+        do{
+            System.out.println("\n1.enqueue\n2.deQueue\n3.print\n0.exit");
+            choice=sc.nextInt();
+            switch (choice) {
+                case 1:
+                if(obj.isFull()!=true){
+                    element=sc.nextInt();
+                    obj.EnQueue(element);
+                    break;
+
+                }
+                else{
+                    System.out.println("Queue is full");
+                    break;
+                }
+                case 2:
+                if(obj.isEmpty()!=true){
+                    
+                    System.out.println( obj.DeQueue());
+                    break;
+
+                }
+                else{
+                    System.out.println("Queue is empty");
+                    break;
+                }
+               
+                
+                case 3:
+                if(obj.isEmpty()!=true){
+                    
+                    obj.printQueue();
+                    break;
+
+                }
+                else{
+                    System.out.println("Queue is empty");
+                    break;
+                }
+                    
+                    
+            
+                default:
+                    break;
+            }
+
+        }while(choice!=0);//condition 
+        
+    }
+    
+}
